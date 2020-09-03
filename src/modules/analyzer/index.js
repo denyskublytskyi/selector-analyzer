02b60a5f7c51ec7811c3d945f7443445a11b3851
Denys Kublytskyi <deniskublitskiy@gmail.com>
2020-09-03 23:06:51 +0300

    :sparkles: Make screenshots bucket private and presign screenshots urls before sending to telegram. Add error handler if one selector tracking failed continue to track others.

--- src/modules/analyzer/index.js
+++ src/modules/analyzer/index.js
@@ -21,201 +21,211 @@ const start = async ({ notificationService, logger }) => {
     });
 
     await asyncEachSeries(selectors, async (selector) => {
-        const opts = {
-            defaultViewport: {
-                deviceScaleFactor: 1,
-                height: 1080,
-                width: 1920,
-            },
-            headless: process.env.NODE_ENV !== "development",
-        };
+        try {
+            const opts = {
+                defaultViewport: {
+                    deviceScaleFactor: 1,
+                    height: 1080,
+                    width: 1920,
+                },
+                headless: process.env.NODE_ENV !== "development",
+            };
 
-        let browser;
+            let browser;
 
-        if (process.env.NODE_ENV === "development") {
-            // eslint-disable-next-line global-require,import/no-extraneous-dependencies
-            const puppeteer = await require("puppeteer");
-            browser = await puppeteer.launch(opts);
-        } else {
-            browser = await chromeLambda.puppeteer.launch({
-                args: chromeLambda.args,
-                executablePath: await chromeLambda.executablePath,
-                ...opts,
+            if (process.env.NODE_ENV === "development") {
+                // eslint-disable-next-line global-require,import/no-extraneous-dependencies
+                const puppeteer = await require("puppeteer");
+                browser = await puppeteer.launch(opts);
+            } else {
+                browser = await chromeLambda.puppeteer.launch({
+                    args: chromeLambda.args,
+                    executablePath: await chromeLambda.executablePath,
+                    ...opts,
+                });
+            }
+
+            const values = [];
+
+            logger.info("Start processing selector", {
+                selector,
             });
-        }
 
-        const values = [];
+            const page = await browser.newPage();
 
-        logger.info("Start processing selector", {
-            selector,
-        });
+            await page.setUserAgent(
+                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
+            );
+            await page.setExtraHTTPHeaders({
+                "Accept-Language": "en;q=0.9",
+            });
 
-        const page = await browser.newPage();
+            await Promise.all([
+                page.goto(selector.url),
+                page.waitForNavigation(),
+            ]);
 
-        await page.setUserAgent(
-            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
-        );
-        await page.setExtraHTTPHeaders({
-            "Accept-Language": "en;q=0.9",
-        });
+            await asyncEachSeries(
+                selector.selectors,
+                async ({ action, wait, value, path }) => {
+                    const context = pickBy(
+                        {
+                            action,
+                            path,
+                            selector,
+                            value,
+                            wait,
+                        },
+                        identity
+                    );
+                    logger.info("Waiting for selector path", context);
+                    // await page.screenshot({ path: `${selector.name}-${path}.png` });
+                    await page.waitForSelector(path);
 
-        await Promise.all([page.goto(selector.url), page.waitForNavigation()]);
+                    if (wait) {
+                        logger.info("Waiting", context);
+                        await page.waitFor(wait);
+                    }
 
-        await asyncEachSeries(
-            selector.selectors,
-            async ({ action, wait, value, path }) => {
-                const context = pickBy(
-                    {
-                        action,
-                        path,
-                        selector,
-                        value,
-                        wait,
-                    },
-                    identity
-                );
-                logger.info("Waiting for selector path", context);
-                // await page.screenshot({ path: `${selector.name}-${path}.png` });
-                await page.waitForSelector(path);
+                    if (action === "getValue") {
+                        const valueElement = await page.$(path);
+                        if (!(await valueElement.isIntersectingViewport())) {
+                            await page.evaluate(
+                                ({ path: selectorPath }) => {
+                                    // eslint-disable-next-line no-undef
+                                    const el = document.querySelector(
+                                        selectorPath
+                                    );
 
-                if (wait) {
-                    logger.info("Waiting", context);
-                    await page.waitFor(wait);
-                }
+                                    if (el) {
+                                        el.scrollIntoView({ block: "center" });
+                                    }
+                                },
+                                { path }
+                            );
+                        }
 
-                if (action === "getValue") {
-                    const valueElement = await page.$(path);
-                    if (!(await valueElement.isIntersectingViewport())) {
                         await page.evaluate(
                             ({ path: selectorPath }) => {
                                 // eslint-disable-next-line no-undef
                                 const el = document.querySelector(selectorPath);
-
-                                if (el) {
-                                    el.scrollIntoView({ block: "center" });
-                                }
+                                el.style.border = "2px solid red";
+                                el.style.padding = "15px";
                             },
                             { path }
                         );
+
+                        logger.info("Making screenshot", context);
+                        const buffer = await page.screenshot();
+
+                        const optimizedBuffer = await imagemin.buffer(buffer, {
+                            plugins: [
+                                imageminPngquant({
+                                    quality: [0.6, 0.8],
+                                }),
+                            ],
+                        });
+
+                        logger.info("Uploading screenshot", context);
+                        const s3Response = await s3
+                            .upload({
+                                ACL: "private",
+                                Body: optimizedBuffer,
+                                Bucket: process.env.S3_BUCKET,
+                                ContentType: "image/png",
+                                Key: `${
+                                    selector.name
+                                }/${new Date().toISOString()}.png`,
+                            })
+                            .promise();
+
+                        logger.info("Screenshot is uploaded", {
+                            ...context,
+                            s3Response,
+                        });
+
+                        logger.info("Get value", context);
+                        const result = await page.$eval(
+                            path,
+                            (element) => element.textContent
+                        );
+
+                        values.push({
+                            screenshotUrl: s3.getSignedUrl("getObject", {
+                                Bucket: process.env.S3_BUCKET,
+                                Key: s3Response.Key,
+                            }),
+                            value: result.replace(/[\t\n\r]/, "").trim(),
+                        });
                     }
 
-                    await page.evaluate(
-                        ({ path: selectorPath }) => {
-                            // eslint-disable-next-line no-undef
-                            const el = document.querySelector(selectorPath);
-                            el.style.border = "2px solid red";
-                            el.style.padding = "15px";
-                        },
-                        { path }
-                    );
+                    if (action === "click") {
+                        logger.info("Click", context);
+                        await page.click(path);
 
-                    logger.info("Making screenshot", context);
-                    const buffer = await page.screenshot();
+                        return;
+                    }
 
-                    const optimizedBuffer = await imagemin.buffer(buffer, {
-                        plugins: [
-                            imageminPngquant({
-                                quality: [0.6, 0.8],
-                            }),
-                        ],
-                    });
-
-                    logger.info("Uploading screenshot", context);
-                    const s3Response = await s3
-                        .upload({
-                            ACL: "public-read",
-                            Body: optimizedBuffer,
-                            Bucket: process.env.S3_BUCKET,
-                            ContentType: "image/png",
-                            Key: `${
-                                selector.name
-                            }/${new Date().toISOString()}.png`,
-                        })
-                        .promise();
-
-                    logger.info("Screenshot is uploaded", {
-                        ...context,
-                        s3Response,
-                    });
-
-                    logger.info("Get value", context);
-                    const result = await page.$eval(
-                        path,
-                        (element) => element.textContent
-                    );
-
-                    values.push({
-                        screenshotUrl: s3Response.Location,
-                        value: result.replace(/[\t\n\r]/, "").trim(),
-                    });
+                    if (action === "type") {
+                        logger.info("Type", context);
+                        await page.click(path, { clickCount: 3 });
+                        await page.type(path, value, { delay: 100 });
+                    }
                 }
+            );
 
-                if (action === "click") {
-                    logger.info("Click", context);
-                    await page.click(path);
+            const value = map(values, "value").join(" ");
+            const lastTrack = await selectorsService.getLastTrack({
+                // eslint-disable-next-line no-underscore-dangle
+                selectorId: selector._id,
+            });
+            logger.info("Last track", { lastTrack, selector });
 
-                    return;
-                }
+            const screenshotsUrls = map(values, "screenshotUrl");
 
-                if (action === "type") {
-                    logger.info("Type", context);
-                    await page.click(path, { clickCount: 3 });
-                    await page.type(path, value, { delay: 100 });
-                }
+            if (!lastTrack) {
+                logger.log(
+                    `Start tracking with value ${JSON.stringify(value)}`,
+                    selector
+                );
+                await notificationService.selectorValueChangeNotify({
+                    newValue: value,
+                    screenshotsUrls,
+                    selector,
+                });
             }
-        );
 
-        const value = map(values, "value").join(" ");
-        const lastTrack = await selectorsService.getLastTrack({
-            // eslint-disable-next-line no-underscore-dangle
-            selectorId: selector._id,
-        });
-        logger.info("Last track", { lastTrack, selector });
+            if (
+                lastTrack &&
+                lastTrack.value !== value &&
+                shouldNotify(value, lastTrack.value, selector.notifyWhen)
+            ) {
+                logger.log(
+                    `Value changes from ${JSON.stringify(
+                        lastTrack.value
+                    )} to ${JSON.stringify(value)}`,
+                    selector
+                );
+                await notificationService.selectorValueChangeNotify({
+                    newValue: value,
+                    oldValue: lastTrack.value,
+                    screenshotsUrls,
+                    selector,
+                });
+            }
 
-        const screenshotsUrls = map(values, "screenshotUrl");
-
-        if (!lastTrack) {
-            logger.log(
-                `Start tracking with value ${JSON.stringify(value)}`,
-                selector
-            );
-            await notificationService.selectorValueChangeNotify({
-                newValue: value,
+            logger.info("Saving track", { selector, value });
+            await selectorsService.saveTrack({
                 screenshotsUrls,
                 selector,
+                value,
             });
+
+            await page.close();
+            await browser.close();
+        } catch (e) {
+            logger.error(e.message, { selector });
         }
-
-        if (
-            lastTrack &&
-            lastTrack.value !== value &&
-            shouldNotify(value, lastTrack.value, selector.notifyWhen)
-        ) {
-            logger.log(
-                `Value changes from ${JSON.stringify(
-                    lastTrack.value
-                )} to ${JSON.stringify(value)}`,
-                selector
-            );
-            await notificationService.selectorValueChangeNotify({
-                newValue: value,
-                oldValue: lastTrack.value,
-                screenshotsUrls,
-                selector,
-            });
-        }
-
-        logger.info("Saving track", { selector, value });
-        await selectorsService.saveTrack({
-            screenshotsUrls,
-            selector,
-            value,
-        });
-
-        await page.close();
-        await browser.close();
-
-        return values;
     });
 };
 
